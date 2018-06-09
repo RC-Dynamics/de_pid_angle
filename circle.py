4a70e9745269292d1fdc167bdf857fcf7dff2116
Roberto Fernandes <betocostaf@gmail.com>
2018-06-09 11:37:17 -0300

    Creating cicle class

---
+++ circle.py
@@ -0,0 +1,17 @@
+import math
+
+class Circle:
+    
+    def __init__(self, r):
+        self.radius = r
+
+    def circle_discretizaztion(self, qtd_poits = 40):
+        ret = []
+        angle_diff = 2 * math.pi / qtd_poits
+        for i in range(qtd_poits):
+            point = (self.radius * math.cos(i * angle_diff), self.radius * math.sin(i * angle_diff))
+            ret.append(point)
+        return ret
+    
+    def area(self):
+        return math.pi * self.radius * self.radius
