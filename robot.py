ac0abb7439b5f33f2ff956697b852bcec9129217
cso <cso@cin.ufpe.br>
2018-06-13 17:15:43 -0300

    Including robot class and main.py

---
+++ robot.py
@@ -0,0 +1,23 @@
+class Robot(object):
+	"""docstring for Robot"""
+	def __init__(self):
+		super(Robot, self).__init__()
+		self.x = 0
+		self.y = 0
+		self.angle = 0
+
+	def setCoord(self,x,y,angle):
+		self.x = x
+		self.y = y
+		self.angle = angle
+		print(self.angle)
+
+	def getCoord(self):
+		return self.x,self.y,self.angle
+
+	def setVel(self, vLeft, vRight):
+		self.vLeft = vLeft
+		self.vRight = vRight
+
+	def getVel(self):
+		return '{"robot0":{"left":'+str(self.vLeft)+',"right":'+str(self.vRight)+'},"robot1":{"left":0,"right":0},"robot2":{"left":0,"right":0} }'
