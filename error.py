d8d9a566c7a1e579c320f95907c32b69d481e9fb
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-06-13 15:25:39 -0300

    inside a class

--- error.py
+++ error.py
@@ -3,36 +3,35 @@ import numpy as np
 from random import random
 import math
 
-def getSimplePathError(path, robotPath, N=1):
-    totalError = 0.0
-    for i in range(len(path) - 1):
-        totalError += getSimpleError(path[i], path[i+1], robotPath[i])[2]
-    
-    totalError += getSimpleError(path[i], path[1], robotPath[i])[2]    
-    return totalError/N
+class PathError:
+    def getSimpleError(self, path, robotPath, N=1):
+        totalError = 0.0
+        for i in range(len(path) - 1):
+            totalError += self.getPointError(path[i], path[i+1], robotPath[i])[2]
+        
+        totalError += self.getPointError(path[i], path[1], robotPath[i])[2]    
+        return totalError/N
 
-def getSimpleError(startPoint, endPoint, robotPos):
-    start, end, robot= np.array(startPoint), np.array(endPoint), np.array(robotPos)
-    middle = (start + end)/ 2.0
-    #cv2.circle(src, (int(middle[0]),int(middle[1])), 3, (255, 255, 255), 1)
-    errorS = np.sqrt(((start - robot) ** 2).sum(0))
-    errorE = np.sqrt(((end - robot) ** 2).sum(0))
-    errorM = np.sqrt(((middle - robot) ** 2).sum(0))
-    return errorS, errorE, errorM
-    
-def tD2I(tup):
-    return (int(tup[0]), int(tup[1]))
+    def getPointError(self, startPoint, endPoint, robotPos):
+        start, end, robot= np.array(startPoint), np.array(endPoint), np.array(robotPos)
+        middle = (start + end)/ 2.0
+        errorS = np.sqrt(((start - robot) ** 2).sum(0))
+        errorE = np.sqrt(((end - robot) ** 2).sum(0))
+        errorM = np.sqrt(((middle - robot) ** 2).sum(0))
+        return errorS, errorE, errorM
+        
+    def tD2I(self, tup):
+        return (int(tup[0]), int(tup[1]))
 
-def getPerimeterError(startPoint, endPoint, robotPath, src=''):
-    path = [startPoint] + robotPath + [endPoint]
-    pathNp = np.array(path)
-    totalError = 0.0
-    for i in range(len (pathNp) - 1):
-        totalError += np.sqrt(((pathNp[i] - pathNp[i+1]) ** 2).sum(0))
-        if src is not '':
-            cv2.arrowedLine(src, tD2I(path[i]), tD2I(path[i+1]), (255,255,255))
-    
-    return totalError
+    def getPerimeterError(self, startPoint, endPoint, robotPath, src=''):
+        path = [startPoint] + robotPath + [endPoint]
+        pathNp = np.array(path)
+        totalError = 0.0
+        for i in range(len (pathNp) - 1):
+            totalError += np.sqrt(((pathNp[i] - pathNp[i+1]) ** 2).sum(0))
+            if src is not '':
+                cv2.arrowedLine(src, tD2I(path[i]), tD2I(path[i+1]), (255,255,255))
+        return totalError
     
 
 if __name__ == '__main__':
@@ -40,7 +39,7 @@ if __name__ == '__main__':
     height, width = 480, 640
     radius = 150
     angleShift = math.radians(20)
-    src = np.zeros(shape=(height, width, 3))
+    src = np.zeros(shape=(height, width, 3),dtype=np.int)
     center = (width/2, height/2)
     cv2.circle(src, center, radius, (0,0,255),1)
     path = []
@@ -66,8 +65,10 @@ if __name__ == '__main__':
         cv2.circle(src, (int(xr), int(yr)), 1, (0, 255, 0), 1)
         
         path.append((x,y))
-        
-    print (getPerimeterError(path[0], path[1], robotPath, src))
+
+    ptError = PathError()
+
+    print (ptError.getPerimeterError(path[0], path[1], robotPath, src))
     
     cv2.namedWindow("ErrorWin", cv2.WINDOW_NORMAL)
     cv2.imshow("ErrorWin",src);
